2d8d7e121d657beb70f1428e9742dd4caad1cf56
Greg <gbasera@gbox.adnu.edu.ph>
2020-01-16 13:05:31 +0800

    Removed boiler plate files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import changeDocView from './collectionsRed';
-import loggedReducer from './isLogged';
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
     docView : changeDocView,
-    logged : loggedReducer
 })
 
 export default allReducers;
