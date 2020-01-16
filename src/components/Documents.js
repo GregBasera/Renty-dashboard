dcdb1aeda5e44d00f777881ddb2b789dcf6b9707
Greg <gbasera@gbox.adnu.edu.ph>
2020-01-16 14:51:38 +0800

    Learned how to conditional render

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
+// Components
+import RentalView from './RentalView';
+
 // Layout
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
 function Documents() {
-  const veiwIndex = useSelector(state => state.docView.index);
   const viewTitle = useSelector(state => state.docView.title);
+  const viewIndex = useSelector(state => state.docView.index);
+
+  const view = (index) => {
+    switch (index) {
+      case 1:
+        return <RentalView />;
+      default:
+        return 0;
+    }
+  }
 
   return (
     <Container maxWidth="md" disableGutters="true" style={{height:"90vh",overflowY:"scroll"}}>
@@ -17,7 +29,7 @@ function Documents() {
       <hr size="1"/>
 
       <Typography variant="h6" style={{marginLeft:"10px"}}>
-        {veiwIndex}
+        {view(viewIndex)}
       </Typography>
     </Container>
   )
