37d48e717a2401de5aa42fcf7769b5dbb2bd0ac8
Greg <gbasera@gbox.adnu.edu.ph>
2020-01-31 01:12:17 +0800

    Added collection name to docView store.

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -35,9 +35,9 @@ function Collections() {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   const dispatch = useDispatch();
 
-  const handleListItemClick = (event, index, title) => {
+  const handleListItemClick = (event, index, title, frbsColl) => {
     setSelectedIndex(index);
-    dispatch(changeDocView(index, title));
+    dispatch(changeDocView(index, title, frbsColl));
   };
 
   return (
@@ -48,77 +48,77 @@ function Collections() {
       <hr size="1"/>
 
       <List component="nav" aria-label="Collections" dense={true}>
-        <ListItem button selected={selectedIndex === 1} onClick={event => handleListItemClick(event, 1, 'All Rentals')} >
+        <ListItem button selected={selectedIndex === 1} onClick={event => handleListItemClick(event, 1, 'All Rentals', 'rentals')} >
           <ListItemIcon>
             <ShoppingBasketIcon />
           </ListItemIcon>
           <ListItemText primary="Rentals" />
           <Chip variant="outlined" size="small" label="100" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 2} onClick={event => handleListItemClick(event, 2, 'Due')} >
+        <ListItem button className={classes.nested} selected={selectedIndex === 2} onClick={event => handleListItemClick(event, 2, 'Due', 'rentals')} >
           <ListItemIcon>
             <ErrorIcon />
           </ListItemIcon>
           <ListItemText primary="Due" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 3} onClick={event => handleListItemClick(event, 3, 'Pending')} >
+        <ListItem button className={classes.nested} selected={selectedIndex === 3} onClick={event => handleListItemClick(event, 3, 'Pending', 'rentals')} >
           <ListItemIcon>
             <HourglassEmptyIcon />
           </ListItemIcon>
           <ListItemText primary="Pending" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 4} onClick={event => handleListItemClick(event, 4, 'Leased')} >
+        <ListItem button className={classes.nested} selected={selectedIndex === 4} onClick={event => handleListItemClick(event, 4, 'Leased', 'rentals')} >
           <ListItemIcon>
             <ExitToAppIcon />
           </ListItemIcon>
           <ListItemText primary="Leased" />
         </ListItem>
 
-        <ListItem button selected={selectedIndex === 5} onClick={event => handleListItemClick(event, 5, 'All Items')} >
+        <ListItem button selected={selectedIndex === 5} onClick={event => handleListItemClick(event, 5, 'All Items', 'items')} >
           <ListItemIcon>
             <DashboardIcon />
           </ListItemIcon>
           <ListItemText primary="Items" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 6} onClick={event => handleListItemClick(event, 6, 'In App')} >
+        <ListItem button className={classes.nested} selected={selectedIndex === 6} onClick={event => handleListItemClick(event, 6, 'In App', 'items')} >
           <ListItemIcon>
             <StoreIcon />
           </ListItemIcon>
           <ListItemText primary="In-App" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 7} onClick={event => handleListItemClick(event, 7, 'For-Approval')} >
+        <ListItem button className={classes.nested} selected={selectedIndex === 7} onClick={event => handleListItemClick(event, 7, 'For-Approval', 'items')} >
           <ListItemIcon>
             <HourglassEmptyIcon />
           </ListItemIcon>
           <ListItemText primary="For-Approval" />
         </ListItem>
 
-        <ListItem button selected={selectedIndex === 8} onClick={event => handleListItemClick(event, 8, 'All Users')} >
+        <ListItem button selected={selectedIndex === 8} onClick={event => handleListItemClick(event, 8, 'All Users', 'users')} >
           <ListItemIcon>
             <GroupIcon />
           </ListItemIcon>
           <ListItemText primary="Users" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 9} onClick={event => handleListItemClick(event, 9, 'Verified')}>
+        <ListItem button className={classes.nested} selected={selectedIndex === 9} onClick={event => handleListItemClick(event, 9, 'Verified', 'users')}>
           <ListItemIcon>
             <VerifiedUserIcon />
           </ListItemIcon>
           <ListItemText primary="Verified" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 10} onClick={event => handleListItemClick(event, 10, 'Pending')}>
+        <ListItem button className={classes.nested} selected={selectedIndex === 10} onClick={event => handleListItemClick(event, 10, 'Pending', 'users')}>
           <ListItemIcon>
             <HourglassEmptyIcon />
           </ListItemIcon>
           <ListItemText primary="Pending" />
         </ListItem>
-        <ListItem button className={classes.nested} selected={selectedIndex === 11} onClick={event => handleListItemClick(event, 11, 'Not Verified')}>
+        <ListItem button className={classes.nested} selected={selectedIndex === 11} onClick={event => handleListItemClick(event, 11, 'Not Verified', 'users')}>
           <ListItemIcon>
             <ErrorOutlineIcon />
           </ListItemIcon>
           <ListItemText primary="Not Verified" />
         </ListItem>
 
-        <ListItem button selected={selectedIndex === 12} onClick={event => handleListItemClick(event, 12, 'Client App')} >
+        <ListItem button selected={selectedIndex === 12} onClick={event => handleListItemClick(event, 12, 'Client App', null)} >
           <ListItemIcon>
             <PhonelinkRingIcon />
           </ListItemIcon>
