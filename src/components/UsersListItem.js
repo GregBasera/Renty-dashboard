1a683f3b05205348996873c0cf839155273b01a7
GregBasera <basera.gg@gmail.com>
2020-01-30 20:20:29 +0800

    Seperated ListItem component from UserView. Gave the ability to push to redux store to listItem

---
+++ src/components/UsersListItem.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { changeFieldView } from '../actions/collectionsActs';
+
+// Layout
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+// Icons
+import PersonIcon from '@material-ui/icons/Person';
+
+function UsersListItem(props) {
+  const [selectedIndex, setSelectedIndex] = React.useState(-1);
+  const dispatch = useDispatch();
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+    dispatch(changeFieldView(index));
+  };
+
+  console.log(props.users);
+  return(
+    <div>
+      {props.users.map((user, index) =>
+        {
+          return (
+            <div>
+            <ListItem button selected={selectedIndex === index} onClick={event => handleListItemClick(event, index)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <PersonIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={user.name} secondary={user.phone} />
+            </ListItem>
+            <Divider />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default UsersListItem;
+
+// <ListItem button selected={selected === index} onClick={() => this.updateSelected(index)}>
--- src/components/UsersView.js
+++ src/components/UsersView.js
@@ -1,23 +1,15 @@
 import React from 'react';
+import UsersListItem from './UsersListItem';
 
 // Layout
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-
-// Icons
-import PersonIcon from '@material-ui/icons/Person';
 
 class UsersView extends React.Component {
   constructor(props) {
     super(props);
-    this.updateSelected = this.updateSelected.bind(this);
     this.state = ({
       users: [],
-      selected: null,
     });
 
     this.listenToFirebase = this.listenToFirebase.bind(this);
@@ -58,33 +50,11 @@ class UsersView extends React.Component {
     }
   }
 
-  updateSelected(selectedIndex) {
-    this.setState({ selected: selectedIndex });
-  }
-
   render () {
-    const { selected } = this.state;
-
     return (
       <List component="nav" aria-label="Collections" dense="true">
         <Divider />
-        {this.state.users.map((user, index) =>
-          {
-            return (
-              <div>
-              <ListItem button selected={selected === index} onClick={() => this.updateSelected(index)}>
-                <ListItemAvatar>
-                  <Avatar>
-                    <PersonIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={user.name} secondary={user.phone} />
-              </ListItem>
-              <Divider />
-              </div>
-            )
-          })
-        }
+        <UsersListItem users={this.state.users}/>
       </List>
     )
   }
