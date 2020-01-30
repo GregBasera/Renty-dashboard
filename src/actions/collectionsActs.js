782057cbd1f8363d19ca71ff62396f5f092118af
Greg <gbasera@gbox.adnu.edu.ph>
2020-01-31 01:09:43 +0800

    Added user_ID to redux store

--- src/actions/collectionsActs.js
+++ src/actions/collectionsActs.js
@@ -1,13 +1,13 @@
-export const changeDocView = (index, title) => {
+export const changeDocView = (index, title, frbsColl) => {
     return {
         type: "docView",
-        payload: { index: index, title: title }
+        payload: { index: index, title: title, frbsColl: frbsColl }
     };
 }
 
-export const changeFieldView = (index, string) => {
+export const changeFieldView = (index, user_id) => {
     return {
       type: "fieldView",
-      payload: { index: index, string: string }
+      payload: { index: index, user_id: user_id }
     };
 }
