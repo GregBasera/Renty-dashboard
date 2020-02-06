73a2338883b89d79d7254a6edaf394e75fc14ee7
GregBasera <basera.gg@gmail.com>
2020-02-06 16:52:34 +0800

    initiating rentals view

--- src/components/DocumentsChildren/RentalsView.js
+++ src/components/DocumentsChildren/RentalsView.js
@@ -1,16 +1,62 @@
 import React from 'react';
 
-function RentalsView(props) {
-  props.query.onSnapshot((snapshot) => {
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-      console.log(change);
-    })
-  })
+import RentalsListItem from './RentalViewChildren/RentalsListItem';
 
-  return(
-    <h1>{"hello"}</h1>
-  )
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+
+class RentalsView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      rentals: [],
+    });
+  }
+
+  listenToFirebase() {
+    var list = [];
+    this.props.query.onSnapshot((snapshot) => {
+      let changes = snapshot.docChanges();
+      changes.forEach(change => {
+        switch(change.type) {
+          case 'added':
+            list.unshift({
+              rent_id: change.doc.id,
+              lender: change.doc.data().lender,
+              renter: change.doc.data().renter,
+            });
+            break;
+          case 'removed':
+            // list = this.state.users;
+            // list.splice(list.indexOf(list.length - change.oldIndex -1), 1);
+            // can delete things without proper arrangeBy function
+            console.log(change.oldIndex);
+            break;
+          case 'modified':
+            console.log("modified");
+            break;
+          default:
+            break;
+        }
+      })
+      this.setState({ items: list });
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(prevProps.query !== this.props.query) {
+      this.listenToFirebase();
+    }
+  }
+
+  render() {
+    return (
+      <List component="nav" aria-label="Collections" dense="true">
+        <Divider />
+        <RentalsListItem rentals={this.state.rentals} />
+      </List>
+    );
+  }
 }
 
 export default RentalsView;
