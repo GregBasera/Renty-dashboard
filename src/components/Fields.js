87fddc23b23893d8cf66ab2c26f6c8061f11f137
GregBasera <basera.gg@gmail.com>
2020-02-06 11:48:05 +0800

    Added refresh button no listener yet. Also spacings.

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -10,6 +10,9 @@ import ItemField from './FieldsChildren/ItemField';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import ReplayIcon from '@material-ui/icons/Replay';
 
 function Fields() {
   const collection = useSelector(state => state.docView.frbsColl);
@@ -29,13 +32,22 @@ function Fields() {
   }
 
   return (
-    <Container maxWidth="md" disableGutters style={{height:"90vh", overflowY:"auto"}}>
-      <Typography variant="h6" style={{marginLeft:"10px"}}>
-        Fields
-      </Typography>
+    <Container maxWidth="md" disableGutters style={{height:"85vh", overflowY:"auto"}}>
+      <Grid container spacing={0}>
+        <Grid item xs={6}>
+          <Typography variant="h6" style={{marginLeft:"10px"}}>
+          Fields
+          </Typography>
+        </Grid>
+          <Grid container xs={6} justify="flex-end">
+            <Button variant="contained" size="small" style={{marginRight:"10px"}}>
+              <ReplayIcon />
+            </Button>
+          </Grid>
+        </Grid>
       <hr size="1"/>
 
-      <Typography variant="h6" style={{marginLeft:"10px", marginTop:"20px"}}>
+      <Typography variant="h6" style={{margin:"0px 10px"}}>
         {view(collection)}
       </Typography>
     </Container>
