ae5b69031f80ac2cf7af07b5f95979fdd0aa52da
Greg <gbasera@gbox.adnu.edu.ph>
2020-02-12 22:23:05 +0800

    index should be unique, this should be applied to other components

--- src/components/DocumentsChildren/ItemViewChildren/ItemsListItem.js
+++ src/components/DocumentsChildren/ItemViewChildren/ItemsListItem.js
@@ -26,9 +26,9 @@ function ItemsListItem(props) {
       {props.items.map((item, index) =>
         {
           return (
-            <div>
-            <ListItem button selected={selectedIndex === index} onClick={event => handleListItemClick(event, index, item.item_id)}>
-              <ListItemText primary={
+            <div key={item.item_id}>
+            <ListItem key={item.item_id} selected={selectedIndex === index} onClick={event => handleListItemClick(event, index, item.item_id)}>
+              <ListItemText key={item.item_id} primary={
                 <Typography variant="h6">
                   {item.item_name}
                 </Typography>
@@ -37,7 +37,7 @@ function ItemsListItem(props) {
                   {(item.is_approved) ? "Approved" : "Not Approved"}
                 </Typography>
               } />
-              <ListItemSecondaryAction>
+              <ListItemSecondaryAction key={item.item_id}>
                 <IconButton edge="end" aria-label="delete">
                   <DeleteIcon />
                 </IconButton>
