21c76e1f15e57329bdf53a005f5383c8d26fa331
GregBasera <basera.gg@gmail.com>
2020-02-17 17:07:18 +0800

    Code Cleaning

--- src/components/FieldsChildren/UserField.js
+++ src/components/FieldsChildren/UserField.js
@@ -11,9 +11,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 
-
-import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
-
 // import UserFieldElements from './UserFieldChildren/UserFieldElements.js';
 // import CircularProgress from '@material-ui/core/CircularProgress';
 
