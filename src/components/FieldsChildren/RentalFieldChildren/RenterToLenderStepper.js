3eefc5d4be7e3f21957611ce4a111d82979c46f6
Greg <gbasera@gbox.adnu.edu.ph>
2020-02-28 06:35:10 +0800

    Tried putting handleNext onClick.

--- src/components/FieldsChildren/RentalFieldChildren/RenterToLenderStepper.js
+++ src/components/FieldsChildren/RentalFieldChildren/RenterToLenderStepper.js
@@ -21,7 +21,7 @@ function RenterToLenderStepper() {
       <Stepper activeStep={activeStep} orientation="vertical" style={{ flexDirection: "column-reverse" }}>
         {steps.map((label, index) => {
           return (
-            <Step key={label}>
+            <Step key={label} onClick={handleNext}>
               <StepLabel>
                 {label}
               </StepLabel>
