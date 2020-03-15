a145adf35395db3eab5797165ee7c54098f7484f
Greg <gbasera@gbox.adnu.edu.ph>
2020-03-16 06:30:17 +0800

    Removed stepper from the parent, made a child to handle it. Ithink im doing the long method here :(

--- src/components/FieldsChildren/RentalField.js
+++ src/components/FieldsChildren/RentalField.js
@@ -14,6 +14,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import UsersCollDialog from './RentalFieldChildren/UsersCollDialog';
 import ItemsCollDialog from './RentalFieldChildren/ItemsCollDialog';
 import TfNoEdit from './RentalFieldChildren/TfNoEdit';
+import StepperFCM from './RentalFieldChildren/StepperFCM';
 // import CircularProgress from '@material-ui/core/CircularProgress';
 
 class RentalField extends React.Component {
@@ -29,9 +30,7 @@ class RentalField extends React.Component {
 
     this.listenToFirebase = this.listenToFirebase.bind(this);
     this.listenToFirebase();
-    this.updateFire = this.updateFire.bind(this);
-    this.stepperNavigation = this.stepperNavigation.bind(this);
-    this.fcm = this.fcm.bind(this);
+    // this.updateFire = this.updateFire.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
@@ -55,14 +54,14 @@ class RentalField extends React.Component {
     }
   }
 
-  updateFire(payload) {
-    let stateRef = this.state.rentalInfo;
-    stateRef.status = payload;
-    this.setState({ rentalInfo: stateRef });
-    this.props.query.update({
-      status: payload,
-    });
-  }
+  // updateFire(payload) {
+  //   let stateRef = this.state.rentalInfo;
+  //   stateRef.status = payload;
+  //   this.setState({ rentalInfo: stateRef });
+  //   this.props.query.update({
+  //     status: payload,
+  //   });
+  // }
 
   peek = (key) => {
     if(this.state.rentalInfo !== null) {
@@ -70,106 +69,6 @@ class RentalField extends React.Component {
       return (Object.keys(returnThis).indexOf(key) !== -1) ? returnThis[key] : "--"
     } else {
       return "--";
-    }
-  }
-
-  fcm(title, body, token) {
-    console.log(token);
-    fetch('https://fcm.googleapis.com/fcm/send', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Authorization' : 'key=AAAAfIlkwIw:APA91bGDIpxkFFsf4hpqnmiQ5OVKexxce8BQ6xbOixXdzXUh_q13WRy6j33vR7VXI-_TJ3ePsU6xRkr044jDhZvkxEZCYjAC9ti2AtYeiTNPdGStrRt-mz3S10K0W8J3i-8JJrG0PnEW',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        // to: 'ceFGfb13aSA:APA91bHD9-Gony4oig3ZxOwGsamb47EZl0U0TqsW_yuKHRoxCQnNqEhYPs2-kUUe_9tU48JEYBYOWzzxPzdWpXd-9epQs8tMYL37Pm2X1ZQwWbH3ikeAME80DgameRQASVnxS3mNOCq5',
-        to: token,
-        notification: {
-          title: title,
-          body: body
-        }
-      })
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('FCM API responce:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  }
-
-  stepperNavigation(index) {
-    switch (index) {
-      case 0:
-        this.fcm(
-          "A person wants to rent an item you listed. [item details]",
-          "We are currently processing the request. You'll be contacted shortly to confirm the item's availability.",
-          this.state.rentalInfo.data.lender_fcm_token
-        ); // lender
-        this.fcm(
-          "Your request is being processed.",
-          "The lender is now being asked for approval.",
-          this.state.rentalInfo.data.renter_fcm_token
-        ); // renter
-        return "The LENDER and RENTER was notified. Contact the lender, discuss and confirm the renting and item details.";
-        // ! ! ! lender can refuse here
-      case 1:
-        return "Dispatch a rider to the Lenders address to retrieve the item.";
-      case 2:
-        return "Item is officially out the hands of the lender and in the hands of Renty. Please inspect the item for any defect. Mark, record, and inform the lender of the seen condition.";
-      case 3:
-        // this.fcm(
-        //   "Item preparation complete.",
-        //   "The item is now on its way to the person who rented it. <renting details>"
-        // ); // lender
-        // this.fcm(
-        //   "Item preparation complete.",
-        //   "The item is now on its way to you."
-        // ); // renter
-        return "The LENDER and RENTER was notified. Dispatch a rider to the Renters address to deliver the rented item.";
-      case 4:
-        return "Item is officially out the hands of Renty and in the hands of the Renter.";
-      case 5:
-        // this.fcm(
-        //   "The item has been received.",
-        //   "The item is due <renting details>. Thank you!"
-        // ); // renter
-        return "The RENTER was notified";
-      case 5:
-        // this.fcm(
-        //   "An item contract just expired.",
-        //   "The renter is now being adviced to prepare the item for retrieval. <item details>"
-        // ); // lender
-        // this.fcm(
-        //   "Your contract just expired.",
-        //   "Please prepare the item for retrieval. A rider will be visiting your address to retrieve the item."
-        // ); // renter
-        return "The LENDER and RENTER was notified.";
-      case 6:
-        return "Dispatch a rider to the Renter's address for retrieval."
-      case 7:
-        // this.fcm(
-        //   "Your item just arrived in the Renty HQ.",
-        //   "Its condition is now being inspected."
-        // ); // lender
-        // this.fcm(
-        //   "Item recieved.",
-        //   "Renty HQ just received the item you just rented. Thank you! <promotions>"
-        // ); // renter
-        return "The LENDER and RENTER was notified. Item is officially out the hands of the Lender and in the hands of Renty.";
-      case 8:
-        return "Dispatch a rider to the Lender's address to return the item."
-      case 9:
-        // this.fcm(
-        //   "Item and Lender reunited.",
-        //   "<promotions>."
-        // ); // Lender
-        return "The LENDER was notified. Item is officially out the hands of Renty and back in the hands of the Lender.";
-      case 10:
-        return "Transactions complete!"
-      default:
-        return null;
     }
   }
 
@@ -184,7 +83,6 @@ class RentalField extends React.Component {
   render() {
     return (this.state.rentalInfo === null) ? <CircularProgress /> : (
       <div>
-        {/*(this.state.rentalInfo) ? <RentalFieldElements info={this.state.rentalInfo} /> : <CircularProgress />*/}
         <Grid container spacing={2}>
           <Grid item xs={12}> {/* item_ID */}
             <TfNoEdit label="Item ID" value={this.state.rentalInfo.data.item_ID} onClick={() => {this.setState({ itemModal: true })}}/>
@@ -199,75 +97,26 @@ class RentalField extends React.Component {
             {/*<UsersCollDialog title="Renter" open={this.state.renterModal} close={this.closeModal} id={this.peek("renter_ID")} coll={"users"}/>*/}
           </Grid>
           <Grid item xs={12}> {/* rental stepper */}
-            <Box borderRadius={4} border={1} borderColor="grey.400" style={{padding:"5px 10px"}}>
+            <StepperFCM
+              query={this.props.query}
+              status={this.state.rentalInfo.data.status}
+              lender_fcm_token={this.state.rentalInfo.data.lender_fcm_token}
+              render_fcm_token={this.state.rentalInfo.data.render_fcm_token}
+            />
+          </Grid>
+          <Grid item xs={6}> {/* duration */}
+            <Box borderRadius={4} border={1} borderColor="grey.400" style={{padding:"5px"}}>
+              <Typography variant="subtitle1" color="textSecondary">
+              Rent Duration
+              </Typography>
               <Grid container spacing={1}>
                 <Grid item xs={12}>
-                  <Grid container spacing={1}>
-                    <Grid item xs={12}>
-                      <Typography variant="subtitle1" color="textPrimary">
-                        {this.stepperNavigation(this.peek("status"))}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6} style={{display: (this.peek("status") === 10) ? "none" : "block"}}>
-                      <Button
-                        fullWidth
-                        disabled={(this.peek("status") === 10) ? true : false}
-                        variant="contained"
-                        color="primary"
-                        onClick={() => {this.updateFire((this.peek("status") === null) ? 0 : this.peek("status")+1)}}
-                      >
-                        {(this.state.rentalInfo.data.status === null) ? 'accept request' : (this.state.rentalInfo.data.status === 9) ? 'finish' : 'next'}
-                      </Button>
-                    </Grid>
-                    <Grid item xs={6} style={{display: (this.peek("status") === 0) ? "block" : "none"}}>
-                      <Button
-                        fullWidth
-                        variant="contained"
-                        color="secondary"
-                        onClick={() => {this.updateFire(-1)}}
-                      >
-                        {"decline"}
-                      </Button>
-                    </Grid>
-                  </Grid>
+                  <TfNoEdit label="Start date" value={this.state.rentalInfo.data.rent_duration.start_date.toDate().toLocaleDateString("en-US", {
+                    year: 'numeric', month: 'short', day: 'numeric', hour:'2-digit', minute:'2-digit' })}/>
                 </Grid>
                 <Grid item xs={12}>
-                  <Stepper activeStep={parseInt(this.peek("status"))} orientation="horizontal" alternativeLabel style={{padding:"10px"}}>
-                    <Step key="Processing Req">
-                      <StepLabel>Processing Req</StepLabel>
-                    </Step>
-                    <Step key="Item to HQ">
-                      <StepLabel>Item to HQ</StepLabel>
-                    </Step>
-                    <Step key="HQ Check">
-                      <StepLabel>HQ Check</StepLabel>
-                    </Step>
-                    <Step key="Item to Renter">
-                      <StepLabel>Item to Renter</StepLabel>
-                    </Step>
-                    <Step key="Renter Received">
-                      <StepLabel>Renter Received</StepLabel>
-                    </Step>
-                  </Stepper>
-                </Grid>
-                <Grid item xs={12}>
-                  <Stepper activeStep={this.peek("status")-6} orientation="horizontal" alternativeLabel style={{padding:"10px"}}>
-                    <Step key="Contract Over">
-                      <StepLabel icon={6}>Contract Over</StepLabel>
-                    </Step>
-                    <Step key="Item to HQ">
-                      <StepLabel icon={7}>Item to HQ</StepLabel>
-                    </Step>
-                    <Step key="HQ Check">
-                      <StepLabel icon={8}>HQ Check</StepLabel>
-                    </Step>
-                    <Step key="Item to Lender">
-                      <StepLabel icon={9}>Item to Lender</StepLabel>
-                    </Step>
-                    <Step key="Lender Received">
-                      <StepLabel icon={10}>Renter Received</StepLabel>
-                    </Step>
-                  </Stepper>
+                  <TfNoEdit label="End date" value={this.state.rentalInfo.data.rent_duration.end_date.toDate().toLocaleDateString("en-US", {
+                    year: 'numeric', month: 'short', day: 'numeric', hour:'2-digit', minute:'2-digit' })}/>
                 </Grid>
               </Grid>
             </Box>
@@ -286,23 +135,6 @@ class RentalField extends React.Component {
                 </Grid>
                 <Grid item xs={12}>
                   <TfNoEdit label="Total Price (₱)" value={this.state.rentalInfo.data.total_price}/>
-                </Grid>
-              </Grid>
-            </Box>
-          </Grid>
-          <Grid item xs={6}> {/* duration */}
-            <Box borderRadius={4} border={1} borderColor="grey.400" style={{padding:"5px"}}>
-              <Typography variant="subtitle1" color="textSecondary">
-                Rent Duration
-              </Typography>
-              <Grid container spacing={1}>
-                <Grid item xs={12}>
-                  <TfNoEdit label="Start date" value={this.state.rentalInfo.data.rent_duration.start_date.toDate().toLocaleDateString("en-US", {
-                    year: 'numeric', month: 'short', day: 'numeric', hour:'2-digit', minute:'2-digit' })}/>
-                </Grid>
-                <Grid item xs={12}>
-                  <TfNoEdit label="End date" value={this.state.rentalInfo.data.rent_duration.end_date.toDate().toLocaleDateString("en-US", {
-                    year: 'numeric', month: 'short', day: 'numeric', hour:'2-digit', minute:'2-digit' })}/>
                 </Grid>
               </Grid>
             </Box>
---
+++ src/components/FieldsChildren/RentalFieldChildren/StepperFCM.js
@@ -0,0 +1,228 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
+function StepperFCM(props) {
+  const stepperToMsg = [0, null, null, 1, null, 2 , 3, null, 4, null, null, 5];
+  const scriptedTitles = [
+    {
+      L: "A person wants to rent an item you listed. [item details]",
+      R: "Your request is being processed."
+    },{
+      L: "Item preparation complete.",
+      R: "Item preparation complete."
+    },{
+      L: "",
+      R: "The item has been received."
+    },{
+      L: "An item contract just expired.",
+      R: "Your contract just expired."
+    },{
+      L: "Your item just arrived in the Renty HQ.",
+      R: "Item recieved."
+    },{
+      L: "Item and Lender reunited.",
+      R: ""
+    }
+  ];
+  const scriptedBodies = [
+    {
+      L: "We are currently processing the request. You'll be contacted shortly to confirm the item's availability.",
+      R: "The lender is now being asked for approval."
+    },{
+      L: "The item is now on its way to the person who rented it. <renting details>",
+      R: "The item is now on its way to you."
+    },{
+      L: "",
+      R: "The item is due <renting details>. Thank you!"
+    },{
+      L: "The renter is now being adviced to prepare the item for retrieval. <item details>",
+      R: "Please prepare the item for retrieval. A rider will be visiting your address to retrieve the item."
+    },{
+      L: "Its condition is now being inspected.",
+      R: "Renty HQ just received the item you just rented. Thank you! <promotions>"
+    },{
+      L: "<promotions>.",
+      R: ""
+    }
+  ];
+
+  const updateStatus = (currStatus) => {
+    props.query.update({
+      status: currStatus,
+    });
+  }
+
+  const fcm = (title, body, token) => {
+    console.log(title, body, token);
+    fetch('https://fcm.googleapis.com/fcm/send', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Authorization' : 'key=AAAAfIlkwIw:APA91bGDIpxkFFsf4hpqnmiQ5OVKexxce8BQ6xbOixXdzXUh_q13WRy6j33vR7VXI-_TJ3ePsU6xRkr044jDhZvkxEZCYjAC9ti2AtYeiTNPdGStrRt-mz3S10K0W8J3i-8JJrG0PnEW',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // to: 'ceFGfb13aSA:APA91bHD9-Gony4oig3ZxOwGsamb47EZl0U0TqsW_yuKHRoxCQnNqEhYPs2-kUUe_9tU48JEYBYOWzzxPzdWpXd-9epQs8tMYL37Pm2X1ZQwWbH3ikeAME80DgameRQASVnxS3mNOCq5',
+        // to: 'fd3Owf5FJms:APA91bFcFrZEDpJ7qPxC9zu1Bf8_m-zluMiJDBUC55JQzAbW2I1SG-KqyX48ca-LnQG-komhqhhIk7pKPcxnQ01nlXetGLdeD6pu-9YHHu1rOw7PEBPyqsO9cnT5unaRsMdEiT26mTqZ'
+        to: token,
+        notification: {
+          title: title,
+          body: body
+        }
+      })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('FCM API responce:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  const fcmButton_L = (index) => {
+    fcm(scriptedTitles[index].L, scriptedBodies[index].L, props.lender_fcm_token);
+  }
+
+  const fcmButton_R = (index) => {
+    fcm(scriptedTitles[index].R, scriptedBodies[index].R, props.lender_fcm_token);
+  }
+
+  const stepperContent = (index) => {
+    switch (index) {
+      case 0: // Processing Req
+        return "The LENDER and RENTER was notified. Contact the lender, discuss and confirm the renting and item details.";
+      case 1: // Item to HQ
+        return "Dispatch a rider to the Lenders address to retrieve the item.";
+      case 2: // HQ Check
+        return "Item is officially out the hands of the lender and in the hands of Renty. Please inspect the item for any defect. Mark, record, and inform the lender of the seen condition.";
+      case 3: // Item to Renter
+        return "The LENDER and RENTER was notified. Dispatch a rider to the Renters address to deliver the rented item.";
+      case 4: // Renter Received
+        return "Item is officially out the hands of Renty and in the hands of the Renter.";
+      case 5: // HIDDEN ########
+        return "The RENTER was notified";
+      case 6: // Contract Over
+        return "The LENDER and RENTER was notified.";
+      case 7: // Item to HQ
+        return "Dispatch a rider to the Renter's address for retrieval."
+      case 8: // HQ Check
+        return "The LENDER and RENTER was notified. Item is officially out the hands of the Lender and in the hands of Renty.";
+      case 9: // Item to Lender
+        return "Dispatch a rider to the Lender's address to return the item."
+      case 10: // Renter Received
+        return "The LENDER was notified. Item is officially out the hands of Renty and back in the hands of the Lender.";
+      case 11: // HIDDEN ########
+        return "Transactions complete!"
+      default:
+        return null;
+    }
+  }
+
+  return(
+    <Box borderRadius={4} border={1} borderColor="grey.400" style={{padding:"5px 10px"}}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Box display={((props.status >= 0 && props.status < 11) && props.status !== null) ? "block" : "none"} borderRadius={4} border={1} borderColor="#fcdfe6"
+                style={{marginTop:"10px", padding:"10px 20px", backgroundColor:"#fcdfe6"}}>
+                <Typography variant="subtitle1" color="textPrimary">
+                  {stepperContent(props.status)}
+                </Typography>
+                <Grid container spacing={1}>
+                  <Grid item xs={6} style={{display: (stepperToMsg[props.status] !== null) ? "block" : "none"}}>
+                    <Button size="small" fullWidth variant="contained" color="primary" onClick={() => {fcmButton_L(stepperToMsg[props.status])}}>
+                      notify Lender
+                    </Button>
+                  </Grid>
+                  <Grid item xs={6} style={{display: (stepperToMsg[props.status] !== null) ? "block" : "none"}}>
+                    <Button size="small" fullWidth variant="contained" color="primary" onClick={() => {fcmButton_R(stepperToMsg[props.status])}}>
+                      notify Renter
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Grid>
+            <Grid item xs={4} style={{display: (props.status === 11) ? "none" : "block"}}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={() => {updateStatus((props.status === null) ? 0 : props.status+1)}}
+              >
+                {(props.status === null) ? 'accept request' : (props.status === 10) ? 'finish' : 'next'}
+              </Button>
+            </Grid>
+            <Grid item xs={4} style={{display: (props.status === 0) ? "block" : "none"}}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="secondary"
+                onClick={() => {updateStatus(-1)}}
+              >
+                {"decline"}
+              </Button>
+            </Grid>
+            <Grid item xs={4} style={{display: ((props.status >= 0 && props.status < 11) && props.status !== null) ? "block" : "none"}}>
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={() => {updateStatus(null)}}
+                style={{backgroundColor:"crimson",color:"white"}}
+              >
+                {"void request"}
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Stepper activeStep={props.status} orientation="horizontal" alternativeLabel style={{padding:"10px"}}>
+            <Step key="Processing Req">
+              <StepLabel>Processing Req</StepLabel>
+            </Step>
+            <Step key="Item to HQ">
+              <StepLabel>Item to HQ</StepLabel>
+            </Step>
+            <Step key="HQ Check">
+              <StepLabel>HQ Check</StepLabel>
+            </Step>
+            <Step key="Item to Renter">
+              <StepLabel>Item to Renter</StepLabel>
+            </Step>
+            <Step key="Renter Received">
+              <StepLabel>Renter Received</StepLabel>
+            </Step>
+          </Stepper>
+        </Grid>
+        <Grid item xs={12}>
+          <Stepper activeStep={props.status-6} orientation="horizontal" alternativeLabel style={{padding:"10px"}}>
+            <Step key="Contract Over">
+              <StepLabel icon={6}>Contract Over</StepLabel>
+            </Step>
+            <Step key="Item to HQ">
+              <StepLabel icon={7}>Item to HQ</StepLabel>
+            </Step>
+            <Step key="HQ Check">
+              <StepLabel icon={8}>HQ Check</StepLabel>
+            </Step>
+            <Step key="Item to Lender">
+              <StepLabel icon={9}>Item to Lender</StepLabel>
+            </Step>
+            <Step key="Lender Received">
+              <StepLabel icon={10}>Renter Received</StepLabel>
+            </Step>
+          </Stepper>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default StepperFCM;
