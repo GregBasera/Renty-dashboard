5a715efb958223956d1ffd79506f2b63026c3b0e
Greg <gbasera@gbox.adnu.edu.ph>
2020-03-30 21:20:19 +0800

    Altered this func a bit. its not producing errors for now.

--- src/components/DocumentsChildren/RentalViewChildren/CountdownChip.js
+++ src/components/DocumentsChildren/RentalViewChildren/CountdownChip.js
@@ -1,12 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
+
+function calculateTimeLeft(end, status) {
+  const difference = end.toDate() - +new Date();
+  let timeLeft = {};
+
+  if (difference > 0 && status === 5) {
+    timeLeft = {
+      d: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      h: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      m: Math.floor((difference / 1000 / 60) % 60),
+      s: Math.floor((difference / 1000) % 60)
+    };
+    return timeLeft;
+  } else {
+    return -1;
+  }
+}
 
 function CountdownChip(props) {
-  
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(props.end, props.status));
 
-  return(
-    <Chip size="small" label={(true) ? "helo" : "Time's up!"} />
-  )
+  useEffect(() => {
+    if(timeLeft !== -1){
+      var clear = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft(props.end, props.status));
+      }, 1000);
+    } else {
+      setTimeLeft("Times up!");
+    }
+
+    return function cleanup() {
+      if(typeof clear !== 'undefined')
+        clearTimeout(clear);
+    };
+  }, [timeLeft, props.end, props.status]);
+
+  // console.log(typeof timeLeft);
+
+  return((typeof timeLeft === "object") ? (
+    <Chip size="small" label={timeLeft.d + "d " + timeLeft.h + "h " + timeLeft.m + "m " + timeLeft.s + "s"}
+      style={{backgroundColor:(timeLeft.d+timeLeft.h === 0 && timeLeft.m <= 15) ? "orange" : "forestgreen",color:"white"}} />
+  ) : (
+    <Chip size="small" label={"Times up!"}
+      style={{backgroundColor:"crimson",color:"white"}} />
+  ));
 }
 
 export default CountdownChip;
